a8e16dd8f9c778977dc32d496f9132bea57724db
Garrick Brazil <garrickbrazil@gmail.com>
2013-03-10 16:54:38 -0400

    Creates bin folder automatically

--- compileMystics.bat
+++ compileMystics.bat
@@ -1,3 +1,7 @@
+@if not exist bin\ (
+	@mkdir bin
+)
+
 @g++ src/MissingPeg.cpp src/DisappearingGrad.cpp src/Circle.cpp src/Arrow.cpp src/Checker.cpp src/BlackDots.cpp src/LinesAndCircle.cpp src/TextDraw.cpp src/Main.cpp -L/lib -L/lib -L/lib -lglut32 -lglu32 -lopengl32 -o bin/Mental_Mystics.exe
 
 @if ERRORLEVEL == 1 ( 
